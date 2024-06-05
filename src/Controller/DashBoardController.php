958faa1b2ddffb98b595dad65b3f9cad3532ba55
Camille K <kempinairecamille@gmail.com>
2024-06-05 17:01:43 +0200

    relecture

--- src/Controller/DashBoardController.php
+++ src/Controller/DashBoardController.php
@@ -17,7 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashBoardController extends AbstractController
 {
-    //calculer le nombre total des membres, groupes, unité et branche pour les afficher sur la page principale
+    //ce que je veux afficher: nombre total membres OK, nombres groupe OK, nombre unité OK, charte branche OK, carte avec nom groupe OK, popup OK
     #[Route('/dashboard', name: 'dashboard')]
     public function index(EntityManagerInterface $entityManager, WsMembreRepository $membreRepository, WsBrancheRepository $brancheRepository, WsUniteRepository $uniteRepository, WsGroupeUniteRepository $groupeUniteRepository, WsSectionRepository $sectionRepository): Response
     {
@@ -30,7 +30,7 @@ class DashBoardController extends AbstractController
         $membresParBranche = $sectionRepository->membresParBranche();
 
         //récupérer le nom de tous les groupes d'unités pour les afficher sur une carte interactive et avoir accés à leur unité
-        //que j'afficherai dans une sideBar
+        //que j'afficherai dans un popUp avec le nom des unités
         $groupeUnite= $entityManager->getRepository(WsGroupeUnite::class)->findAll();
         
        
@@ -46,7 +46,6 @@ class DashBoardController extends AbstractController
             'totalMembres' => $totalMembers, //total membres
             'totalUnite' => $totalUnits, //total d'unité
             'totalGroupeUnite' => $totalGroups, //total de groupe d'unité
-            'totalBranche' => $totalBranche, //le nombre de branche
             'membresParBranche' => $membresParBranche, //le nombre de membres par branches pour les afficher en %
             'groupeUnite' => $groupeUnite, //les groupes d'unité
     
--- src/Controller/WsUniteController.php
+++ src/Controller/WsUniteController.php
@@ -14,18 +14,18 @@ class WsUniteController extends AbstractController
     #[Route('/wsunite/{id}', name: 'wsUnite', methods: ['GET', 'POST'])]
     public function index($id, WsUnite $WsUnite, WsSectionRepository $sectionRepository, WsUniteRepository $uniteRepository): Response
     {
-       //je récupère l'id de l'unité en question pour calculer le nombre de membre
+       //je récupère l'id de l'unité en question pour calculer le nombre de membre vue que je passe en paramètre
        $unite = $uniteRepository->find($id);
 
        if (!$unite) {
            throw $this->createNotFoundException('Unité non trouvée');
        }
 
-       // Utiliser la méthode du repository pour obtenir le nombre de membres
+       // j'utilise la méthode du repository pour obtenir le nombre de membres
        $nombreMembres = $sectionRepository->countMembersByUnite($id);
        $branches = $sectionRepository->membresParBrancheDansUnite($id);
 
-       // Passer l'unité et le nombre de membres à la vue
+       // je passe  l'unité et le nombre de membres à la vue pour les afficher
        return $this->render('ws_unite/index.html.twig', [
            'unite' => $unite,
            'nombreMembres' => $nombreMembres,
