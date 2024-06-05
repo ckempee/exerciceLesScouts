d18a09b4154f1ee67e68e342470e4c3095b00cf6
Camille K <kempinairecamille@gmail.com>
2024-06-05 09:07:30 +0200

    amélioration map groupeUnite

--- src/Controller/DashBoardController.php
+++ src/Controller/DashBoardController.php
@@ -7,8 +7,11 @@ use App\Repository\WsBrancheRepository;
 use App\Repository\WsUniteRepository;
 use App\Repository\WsGroupeUniteRepository;
 use App\Repository\WsSectionRepository;
+use App\Entity\WsGroupeUnite;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,7 +19,7 @@ class DashBoardController extends AbstractController
 {
     //calculer le nombre total des membres, groupes, unité et branche pour les afficher sur la page principale
     #[Route('/dashboard', name: 'app_dash_board')]
-    public function index(WsMembreRepository $membreRepository, WsBrancheRepository $brancheRepository, WsUniteRepository $uniteRepository, WsGroupeUniteRepository $groupeUniteRepository, WsSectionRepository $sectionRepository): Response
+    public function index(EntityManagerInterface $entityManager, WsMembreRepository $membreRepository, WsBrancheRepository $brancheRepository, WsUniteRepository $uniteRepository, WsGroupeUniteRepository $groupeUniteRepository, WsSectionRepository $sectionRepository): Response
     {
         $totalMembers = $membreRepository->count([]);
         $totalUnits = $uniteRepository->count([]);
@@ -26,12 +29,11 @@ class DashBoardController extends AbstractController
         // calculer le nombre de membres par branches(baladins, louveteaux,etc...) gravce a la méthode créée dans le repository
         $membresParBranche = $sectionRepository->membresParBranche();
 
-        //récupérer le nom de tous les groupes d'unités pour les afficher sur une carte interactive
-        $groupNames = $groupeUniteRepository->findAllGroupeUniteNames();
+        //récupérer le nom de tous les groupes d'unités pour les afficher sur une carte interactive et avoir accés à leur unité
+        //que j'afficherai dans une sideBar
+        $groupeUnite= $entityManager->getRepository(WsGroupeUnite::class)->findAll();
         
-        //calculer le nombre d"unité dans chaque groupe
-
-        $unitesParGroupe = $groupeUniteRepository->countUnitesInGroupe();
+       
 
        
         //calcul du pourcentage de membres 
@@ -41,13 +43,13 @@ class DashBoardController extends AbstractController
         }
 
         return $this->render('dash_board/index.html.twig', [
-            'totalMembres' => $totalMembers,
-            'totalUnite' => $totalUnits,
-            'totalGroupeUnite' => $totalGroups,
-            'totalBranche' => $totalBranche,
-            'membresParBranche' => $membresParBranche,
-            'groupNames' => $groupNames,
-            'unitesParGroupe' => $unitesParGroupe,
+            'totalMembres' => $totalMembers, //total membres
+            'totalUnite' => $totalUnits, //total d'unité
+            'totalGroupeUnite' => $totalGroups, //total de groupe d'unité
+            'totalBranche' => $totalBranche, //le nombre de branche
+            'membresParBranche' => $membresParBranche, //le nombre de membres par branches pour les afficher en %
+            'groupeUnite' => $groupeUnite, //les groupes d'unité
+    
            
 
         ]);
